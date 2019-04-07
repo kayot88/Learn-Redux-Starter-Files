cac717f461f08d0cede80afa3f0eafe60b91832f
Kayot88 <kayot88@gmail.com>
2019-04-07 22:02:08 +0300

    Displaying the Single Photo Component

---
+++ learn-redux/client/components/Comments.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Comments extends Component {
+  render() {
+    return (
+      <div>
+        im the comment
+      </div>
+    );
+  }
+}
+
+export default Comments;
--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
-
+import Photo from './Photo';
+import Comments from './Comments';
 class Single extends React.Component {
   render() {
-    return <div className="single-grid">i`m a single grid</div>;
+    //index of the post
+    const i = this.props.posts.findIndex(
+      (post) => post.code === this.props.params.postId
+    );
+    const post = this.props.posts[i];
+    console.log(post);
+    //get us the post
+    return (
+      <div className="single-photo">
+        <Photo i={i} post={post} {...this.props} />
+        <Comments />
+      </div>
+    );
   }
 }
 
