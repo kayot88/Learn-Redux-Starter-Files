af837f261f1b257c7dd5a30a864bcf4130638137
Kayot88 <kayot88@gmail.com>
2019-04-08 17:15:59 +0300

    Updating Comment State in our Store

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -12,11 +12,23 @@ class Comments extends Component {
       </div>
     );
   }
+  submitHandler(e) {
+    e.preventDefault();
+    // console.log('submiting the form');
+    const { postId } = this.props.params;
+    const author = this.refs.author.value;
+    const comment = this.refs.comment.value;
+    this.props.addComment(postId, author, comment);
+  }
   render() {
     return (
       <div>
         {this.props.postComments.map(this.renderComments)}
-        <form ref="commentForm" className="comment-form">
+        <form
+          ref="commentForm"
+          className="comment-form"
+          onSubmit={this.submitHandler.bind(this)}
+        >
           <input type="text" ref="author" placeholder="author" />
           <input type="text" ref="comment" placeholder="comment" />
           <input type="submit" hidden />
